3e3453824591751c8d4622ea272d719aa8f84716
Quintony1 <nyashajerahuni@gmail.com>
2023-01-13 07:29:42 +0000

    airbnb

---
+++ models/state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" State Module for HBNB project """
+import shlex
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from models.city import City
+
+
+class State(BaseModel, Base):
+    """ State class """
+
+     __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
+
+     if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
